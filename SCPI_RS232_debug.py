ec4f92a204e52306c617b9a5b0029d410421447c
ayoungengr <ayoungengr@gmail.com>
2026-03-02 13:29:57 -0800

    reorganized test functions to the debug script

--- SCPI_RS232_debug.py
+++ SCPI_RS232_debug.py
@@ -1,16 +1,26 @@
 import pyvisa
+import time
 import platform
 import re
+cmds = (
+    '*IDN?',
+    'SYST:CONS:BAUD?',
+    'SYST:CONS:EOS?'
+    'MEAS:CURR?'
+    'MEAS:VOLT?'
+    'VOLT:LEV:IMM?'
+)
+bauds = (2400, 4800, 9600, 19200, 38400, 57600, 115200)
+terms = ('', '\n', '\r', '\n\r', '\r\n')
 
 rm = pyvisa.ResourceManager()
-print(rm.list_resources())
-
+reslist = rm.list_resources()
 try:
-    pd = rm.open_resource('ASRL3::INSTR')
+    pd = rm.open_resource(reslist[0])
 except:
     print('connection error')
-bauds = (2400, 4800, 9600, 19200, 38400, 57600, 115200)
-terms = ('','\n','\r','\n\r','\r\n')
+
+#attempt to ping device with all combinations of baud rate, write termination, and read termination
 for i in bauds:
     pd.baud_rate = i
     for wterm in terms:
@@ -25,10 +35,9 @@ for i in bauds:
                 print('other error for baud=' +str(i) + ', wterm=' + str(wterm) + ', rterm=' + str(rterm))
 
 
-# PD8-7 debug mode displays rx memory as 0255 0255 0255 0255 (etc) where 0001-0127 are converted to ASCII and 0000, 0128-0255 remain as-is
+# PD8-7 debug mode displays rx memory as 0255 0255 0255 0255 (etc) where 0001-0127 are converted to ASCII, 0128-0255 remain as-is
 # how can this be parsed to relate back to the literals sent over the serial cable by Python?
 # written string to serial values:
 msg = '*IDN?'.encode('ascii')
 for n in range(len(msg)):
     print(msg[n])
-for
